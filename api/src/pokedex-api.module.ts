fc7ac45018affe467b28a4a85f68ddcec8891de2
Leonardo Oliveira <leonardo.m.oliveira98@gmail.com>
2025-05-16 12:38:39 -0300

    fix providers import

--- api/src/pokedex-api.module.ts
+++ api/src/pokedex-api.module.ts
@@ -3,10 +3,12 @@ import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { PokemonController } from './pokemon/pokemon.controller';
 import { PokemonService } from './pokemon/pokemon.service';
+import { FetchPokemonPaginatedListUseCase } from './pokemon/use_cases/fetch-pokemon-paginated-list.use-case';
+import { FetchPokemonDetailsUseCase } from './pokemon/use_cases/fetch-pokemon-details.use-case';
 
 @Module({
   imports: [HttpModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [PokemonController],
-  providers: [PokemonService],
+  providers: [PokemonService, FetchPokemonPaginatedListUseCase, FetchPokemonDetailsUseCase],
 })
 export class PokedexApiModule {}
